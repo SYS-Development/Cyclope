d5b66de33ab99649bb7585b9ff10510366a6a015
Sam Bonnard <sbonnard06@gmail.com>
2020-02-03 01:18:53 +0100

    V1.0

--- Code /Cyclope-EDU/Cyclope-EDU.ino
+++ Code /Cyclope-EDU/Cyclope-EDU.ino
@@ -115,14 +115,14 @@ enum chan_order {
   AILERON,
   ELEVATOR,
   RUDDER,
-  AUX1,  // (CH5)  led light, or 3 pos. rate on CX-10, H7, or inverted flight on H101
-  AUX2,  // (CH6)  flip control
-  AUX3,  // (CH7)  still camera (snapshot)
-  AUX4,  // (CH8)  video camera
-  AUX5,  // (CH9)  headless
-  AUX6,  // (CH10) calibrate Y (V2x2), pitch trim (H7), RTH (Bayang, H20), 360deg flip mode (H8-3D, H22)
-  AUX7,  // (CH11) calibrate X (V2x2), roll trim (H7)
-  AUX8,  // (CH12) Reset / Rebind
+  AUX1,  
+  AUX2,  
+  AUX3,  
+  AUX4,  
+  AUX5,  
+  AUX6, 
+  AUX7,  
+  AUX8,  
 };
 
 uint8_t transmitterID[4];
